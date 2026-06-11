f6ecc00e2e599f515912bac19dc65a406e54307d
Ryan <ryantonypereira@gmail.com>
2026-06-11 11:26:46 -0500

    Add IP locator request helpers
    Introduce a new module providing simple helpers to fetch the client public IP and geolocation data. get_ip_address() scrapes https://ipchicken.com/ (BeautifulSoup) to return the public IP; get_additional_ip_location() is intended to query ip-api.com and return JSON. Note: the ip-api URL currently contains a malformed placeholder ("http://ip-api.com/json/{ip}}") and should be updated to accept/format the IP argument; imports and comments also contain redundancies and should be cleaned up in a follow-up.

---
+++ api-projects/where-am-i-locator-project/where_ami_i_locator_requests.py
@@ -0,0 +1,54 @@
+#Name: Ryan Pereira
+#Project Name: Where Am I Locator Project
+#Description: 
+#Module Name: main.py
+#Module Purpose: 
+#Collaborators: None
+#Sources:  Github Copilot, Stack Overflow, ChatGPT, Python documentation
+#Date: 6/10/2026
+#Last Modified: 6/11/2026
+
+
+
+
+#Headers setting the user agent to Mozilla/5.0 .
+headers = {"User-Agent": "Mozilla/5.0"}
+#From bs4 module imporrting the BeautifulSoup class.
+from bs4 import BeautifulSoup
+#From requests module importing.
+from requests import *
+#Importing the requests module
+import requests
+#Importing the sys module.
+import sys
+#From datetime, importing the datetime module information
+from datetime import datetime,timezone
+sys.stdout.reconfigure(encoding="utf-8")
+
+
+
+# Retrieves all zodiac sign links from the homepage.
+def get_ip_address():
+    # Stores the URL of the zodiac sign homepage.
+    url = "https://ipchicken.com/"
+    # Sends an HTTP GET request to the website.
+    response = requests.get(url, headers=headers)
+    # Parses the HTML content of the webpage.
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # Returns all zodiac sign link elements.
+    return soup.find("span",id="public-ip").text
+
+
+
+# Retrieves all zodiac sign links from the homepage.
+def get_additional_ip_location():
+    # Stores the URL of the zodiac sign homepage.
+    url = "http://ip-api.com/json/{ip}}"
+    # Sends an HTTP GET request to the website.
+    response = requests.get(url, headers=headers)
+    # Parses the HTML content of the webpage.
+
+    # Returns all zodiac sign link elements.
+    return response.json()
+
+
