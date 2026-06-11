6b41a1a9661baff110e1f9d851aa2f3afac09951
Ryan <ryantonypereira@gmail.com>
2026-06-11 15:51:24 -0500

    Implement WhereAmILocatorBrain methods
    Populate WhereAmILocatorBrain: add imports for IP/location requests and timezone handling; implement show_ip (returns IP), show_location, show_coordinate_location, show_time_zone (formats local time), show_isp, and save_report (writes a summary file). Update play_program to use a menu-driven loop and adjust run to launch the interactive flow. Also update file header metadata and update compiled bytecode. Note: save_report and some print/write calls contain incorrect call signatures and a stray parenthesis that should be fixed in a follow-up.

--- api-projects/where-am-i-locator-project/where_am_i_locator_brain.py
+++ api-projects/where-am-i-locator-project/where_am_i_locator_brain.py
@@ -1,13 +1,17 @@
-#Name: 
-#Project Name:
+#Name: Ryan Pereira
+#Project Name: Where Am I Locator Project
 #Description: 
-#Module Name: 
+#Module Name: main.py
 #Module Purpose: 
-#Collaborators: 
-#Sources: 
-#Date:
-#Last Modified: 
+#Collaborators: None
+#Sources:  Github Copilot, Stack Overflow, ChatGPT, Python documentation
+#Date: 6/10/2026
+#Last Modified: 6/11/2026
 
+
+from where_ami_i_locator_requests import get_ip_address,get_additional_ip_location
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 class WhereAmILocatorBrain:
 
@@ -38,35 +42,98 @@ class WhereAmILocatorBrain:
 
 
     def show_ip(self):
-        pass
+        return get_ip_address()
+
 
     def show_location(self):
-        pass
+        ip_address =  get_ip_address()
+        other_location_infromation = get_additional_ip_location(ip_address)
+
+        print("Country: ",other_location_infromation["country"])
+        print("Region: ",other_location_infromation["regionName"])
+        print("City: ",other_location_infromation["city"])
+        print("Zip Code: ",other_location_infromation["zip"])
+
+    def show_coordinate_location(self):
+        ip_address =  get_ip_address()
+        other_location_infromation = get_additional_ip_location(ip_address)
+
+        print("Latitude: ",other_location_infromation["lat"])
+        print("Longitude: ",other_location_infromation["lon"])
+  
+
 
     def show_time_zone(self):
-        pass
+        ip_address =  get_ip_address()
+        other_location_infromation = get_additional_ip_location(ip_address)
+
+
+        t_time = datetime.now(ZoneInfo(other_location_infromation["timezone"]))
+        formatted_time = t_time.strftime("%I:%M %p")
+
+
+
+
+        print("Time Zone: ",other_location_infromation["timezone"])
+        print("Time Display: ",formatted_time)
 
     def show_isp(self):
-        pass
+        ip_address =  get_ip_address()
+        other_location_infromation = get_additional_ip_location(ip_address)
+
+
+        print("ISP: ",other_location_infromation["isp"])
+        print("Organization: ",other_location_infromation["org"])
+        print("Autonomous System Number",other_location_infromation["as"]))
 
     def save_report(self):
-        pass
+        ip_address =  get_ip_address()
+        other_location_infromation = get_additional_ip_location(ip_address)
+
+        t_time = datetime.now(ZoneInfo(other_location_infromation["timezone"]))
+        formatted_time = t_time.strftime("%I:%M %p")
+
+
+
+
+        
+
+       
+
+      
+      
+
+        with open("where_am_i_reports.txt","w") as report_file:
+            report_file.write("Country: ",other_location_infromation["country"])
+            report_file.write("City: ",other_location_infromation["city"])
+            report_file.write("Zip Code: ",other_location_infromation["zip"])
+            report_file.write("Latitude: ",other_location_infromation["lat"])
+            report_file.write("Longitude: ",other_location_infromation["lon"])
+            report_file.write("Time Zone: ",other_location_infromation["timezone"])
+            report_file.write("Time Display: ",formatted_time)
+            report_file.write("ISP: ",other_location_infromation["isp"])
+            report_file.write("Organization: ",other_location_infromation["org"])
+            report_file.write("Autonomous System Number",other_location_infromation["as"])
+
 
 
     def play_program(self):
         while(True):
+            self.display_menu()
             user_choice = input("\nInput in choice: ")
             if user_choice == "1":
-                user_isp = self.show_isp()
-            if user_choice == "2":
+                user_isp = self.show_ip()
+                print(f"IP ADDRESS: {user_isp}")
+
+            elif user_choice == "2":
                 user_location = self.show_location()
-            if user_choice == "3":
+            elif user_choice == "3":
                 user_time_zone = self.show_time_zone()
-            if user_choice == "4":
+            elif user_choice == "4":
                 user_isp = self.show_isp()
-            if user_choice == "5":
+            elif user_choice == "5":
                 saved_report = self.save_report()
-            if user_choice == "6":
+            elif user_choice == "6":
                 break
             else:
                 print("\nPlease Input a valid choice")
@@ -75,5 +142,10 @@ class WhereAmILocatorBrain:
 
     def run(self) -> None:
         self.display_welcome_message()
-        self.display_menu()
+        self.play_program()
         self.display_goodbye_message()
+
+
+
+
+
